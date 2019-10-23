6f3dd2a3f3595324715f0724ca85a626f7725382
Alexandra Diem <alexandra.diem@gmail.com>
2019-10-23 09:53:09 +0200

    skeleton for physics parameter setup

--- m3h3/__init__.py
+++ m3h3/__init__.py
@@ -1,4 +1,6 @@
 import m3h3.setup_parameters
-from m3h3.setup_parameters import parameters
+from m3h3.setup_parameters import (parameters, set_electro_default_parameters,
+                    set_solid_default_parameters, set_fluid_default_parameters,
+                    set_porous_default_parameters)
 
 setup_parameters.set_dolfin_compiler_parameters()
--- m3h3/setup_parameters.py
+++ m3h3/setup_parameters.py
@@ -7,9 +7,36 @@ parameters = df.Parameters("M3H3")
 def set_dolfin_compiler_parameters():
     """Sets dolfin parameters to speed up the compiler.
     """
-
     flags = ["-O3", "-ffast-math", "-march=native"]
     df.parameters["form_compiler"]["quadrature_degree"] = 4
     df.parameters["form_compiler"]["representation"] = "uflacs"
     df.parameters["form_compiler"]["cpp_optimize"] = True
     df.parameters["form_compiler"]["cpp_optimize_flags"] = " ".join(flags)
+
+
+def set_electro_default_parameters():
+    """Sets default parameters for electrophysiology problems.
+    """
+    electro = df.Parameters("Electro")
+    parameters.add(electro)
+
+
+def set_solid_default_parameters():
+    """Sets default parameters for solid mechanics problems.
+    """
+    solid = df.Parameters("Solid")
+    parameters.add(solid)
+
+
+def set_fluid_default_parameters():
+    """Sets default parameters for fluid dynamics problems.
+    """
+    fluid = df.Parameters("Fluid")
+    parameters.add(fluid)
+
+
+def set_porous_default_parameters():
+    """Sets default parameters for porous mechanics problems.
+    """
+    porous = df.Parameters("Porous")
+    parameters.add(porous)
