33e6ce47dee45c6b722cf5541efa92caff0e1e32
Alexandra Diem <alexandra.diem@gmail.com>
2019-10-23 11:48:27 +0200

    import Physics

--- m3h3/__init__.py
+++ m3h3/__init__.py
@@ -1,8 +1,9 @@
-import m3h3.setup_parameters
-from m3h3.setup_parameters import (parameters, set_electro_default_parameters,
-                    set_solid_default_parameters, set_fluid_default_parameters,
-                    set_porous_default_parameters)
+from m3h3.setup_parameters import (parameters, set_dolfin_compiler_parameters,
+                set_electro_default_parameters, set_solid_default_parameters,
+                set_fluid_default_parameters, set_porous_default_parameters,
+                Physics)
 
 from m3h3.interaction import Interaction
+from m3h3.m3h3 import M3H3
 
-setup_parameters.set_dolfin_compiler_parameters()
+set_dolfin_compiler_parameters()
