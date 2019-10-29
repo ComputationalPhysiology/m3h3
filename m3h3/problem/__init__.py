dc510cd1c9d91eafdcc3738c886e5e2ce156f0af
Alexandra Diem <alexandra.diem@gmail.com>
2019-10-29 12:48:55 +0100

    Skeleton for Problem class and subclasses

---
+++ m3h3/problem/__init__.py
@@ -0,0 +1,5 @@
+from m3h3.problem import problem
+from m3h3.problem import electro_problem
+from m3h3.problem import solid_problem
+from m3h3.problem import fluid_problem
+from m3h3.problem import porous_problem
---
+++ m3h3/problem/problem.py
@@ -0,0 +1,6 @@
+import dolfin as df
+
+class Problem(object):
+
+    def __init__(self):
+        pass
