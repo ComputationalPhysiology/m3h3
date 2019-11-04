e82eb575703ee755e64f1caf37b212101eb78961
Alexandra Diem <alexandra.diem@gmail.com>
2019-11-04 14:07:47 +0100

    cbcbeat wrapper working

--- m3h3/problem/solid_problem.py
+++ m3h3/problem/solid_problem.py
@@ -6,4 +6,4 @@ from m3h3.problem.problem import Problem
 class SolidProblem(Problem):
 
     def __init__(self, geometry, parameters, *args, **kwargs):
-        super().__init__(geometry, *args, **kwargs)
+        pass
