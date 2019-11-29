57d7cbcacb14322765b22b9ee65b34a97553d8a2
Alexandra Diem <alexandra.diem@gmail.com>
2019-11-29 15:21:33 +0100

    added boundary conditions

--- m3h3/problem/solid_problem.py
+++ m3h3/problem/solid_problem.py
@@ -1,24 +1,62 @@
 import dolfin as df
 
 import pulse
+from pulse.utils import get_lv_marker
 
 from m3h3.problem.problem import Problem
 
 
-class SolidProblem(Problem):
+class SolidProblem(pulse.MechanicsProblem):
 
     def __init__(self, geometry, time, parameters, **kwargs):
-        super().__init__(geometry, time, parameters, **kwargs)
-        self._form = self._init_form()
+        bcs_parameters = parameters["BoundaryConditions"]
+        self._set_boundary_conditions(**bcs_parameters)
+        super().__init__(geometry, kwargs['material'])
 
 
-    def _init_form(self):
-        return 0
+    def _set_boundary_conditions(self, **bcs_parameters):
+        # Neumann BCs
+        lv_pressure = pulse.NeumannBC(
+                            traction=Constant(0.0, name="lv_pressure"),
+                            marker=self.geometry.get_lv_marker(), name="lv"
+                        )
+        neumann_bc = [lv_pressure]
 
+        if self.geometry.has_rv():
+            rv_pressure = pulse.NeumannBC(
+                                traction=Constant(0.0, name="rv_pressure"),
+                                marker=self.geometry.get_rv_marker(), name="rv"
+                            )
+            neumann_bc += [rv_pressure]
 
-    def set_initial_conditions(self):
-        pass
+        # Robin BC
+        robin_bc = []
+        if pericardium_spring > 0.0:
+            robin_bc += [pulse.RobinBC(
+                            value=dolfin.Constant(pericardium_spring),
+                            marker=self.geometry.get_epi_marker()
+                        )]
 
+        if base_spring > 0.0:
+            robin_bc += [pulse.RobinBC(
+                            value=dolfin.Constant(base_spring),
+                            marker=self.geometry.get_base_marker()
+                        )]
 
-    def set_material(self):
-        pass
+        # Dirichlet BC
+        if base_bc == "fixed":
+            dirichlet_bc = [partial(
+                                dirichlet_fix_base, ffun=self.geometry.ffun,
+                                marker=self.geometry.get_base_marker(),
+                            )]
+
+        elif base_bc == "fix_x":
+            dirichlet_bc = [partial(
+                                dirichlet_fix_base_directional,
+                                ffun=self.geometry.ffun,
+                                marker=self.geometry.get_base_marker()
+                            )]
+
+        self.bcs = pulse.BoundaryConditions(
+            dirichlet=dirichlet_bc, neumann=neumann_bc, robin=robin_bc
+        )
