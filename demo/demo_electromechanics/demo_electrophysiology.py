6effd0d0ee6ae07e49ebf1c049253e8dced26ae1
Alexandra Diem <alexandra.diem@gmail.com>
2020-01-27 09:42:20 +0100

    move stimulus into parameter set

--- demo/demo_electromechanics/demo_electrophysiology.py
+++ demo/demo_electromechanics/demo_electrophysiology.py
@@ -17,7 +17,7 @@ import numpy as np
 
 import m3h3
 from m3h3 import M3H3, Parameters, Physics, Stimulus
-from geometry import Geometry2D
+from geometry import Geometry2D, MarkerFunctions2D
 
 
 comm = df.MPI.comm_world
@@ -31,16 +31,30 @@ dx = 0.5
 N = lambda v: int(np.rint(v))
 mesh = df.RectangleMesh(comm, df.Point(0.0, 0.0), df.Point(Lx, Ly),
                             N(Lx/dx), N(Ly/dx))
-geo = Geometry2D(mesh)
+
+# Setup stimulation protocol
+class PacingCells(df.SubDomain):
+    def inside(self, x, on_boundary):
+        return pow(x[0], 2) + pow(x[1], 2) < 1.0
+
+markers = {'STIMULUS': 1, 'NONE': 0}
+mf = df.MeshFunction('size_t', mesh, mesh.geometric_dimension()-1)
+mf.set_all(0)
+pacing_cells = PacingCells()
+pacing_cells.mark(mf, markers['STIMULUS'])
+markerfunctions = MarkerFunctions2D(ffun=mf)
+
+geo = Geometry2D(mesh, markers=markers, markerfunctions=markerfunctions)
 
 # Setup parameters
 parameters = Parameters('M3H3')
-parameters['end_time'] = 400.0 # ms
+parameters['end_time'] = 800.0 # ms
 
 parameters.set_electro_parameters()
-eparam = parameters['Electro']
+eparam = parameters[str(Physics.ELECTRO)]
 eparam['M_i'] = 1.0
 eparam['M_e'] = 2.0
+eparam['I_s'].update({'period': 400, 'amplitude': 100})
 eparam['dt'] = 1e-3
 
 starttime = parameters['start_time']
@@ -49,34 +63,24 @@ dt = eparam['dt']
 time = df.Constant(starttime)
 steps = int((endtime-starttime)/dt)
 
-# Setup stimulation protocol
-class PacingCells(df.SubDomain):
-    def inside(self, x, on_boundary):
-        return pow(x[0], 2) + pow(x[1], 2) < 1.0
-
-pacing_markers = df.MeshFunction('size_t', geo.mesh, geo.dim()-1)
-pacing_markers.set_all(0)
-pacing_cells = PacingCells()
-pacing_cells.mark(pacing_markers, 1)
+# Setup simulation
+model = M3H3(geo, parameters, time=time)
 
 # Setup simulation protocol
-physics = [Physics.ELECTRO]
 stim_period = 400
 stimulus = Stimulus(pacing_markers, amplitude=100, period=stim_period,
                                                 duration=5, t=time, degree=1)
-
-# Setup simulation
-m = M3H3(geo, physics, parameters, time=time, stimulus=stimulus)
+model.add_stimulus(stimulus)
 
 # File for output
-f = df.XDMFFile(comm, "demo.xdmf")
+f = df.XDMFFile(comm, "demo_electrophysiology.xdmf")
 f.parameters["flush_output"] = True
 f.parameters["rewrite_function_mesh"] = True
 f.parameters["functions_share_mesh"] = True
 
 # Loop over time
 for step in range(steps):
-    time, fields = m.step()
+    time, fields = model.step()
     if time % 10 < dt:
         for field in fields:
             if field is not None:
